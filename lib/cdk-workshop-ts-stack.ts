a7b820fa3e9ee88ea0792468e89098ac1f26a805
Rene Medalla <renemed@pop-os.localdomain>
2022-08-17 10:49:50 -0400

    added dynamodb table viewer

--- lib/cdk-workshop-ts-stack.ts
+++ lib/cdk-workshop-ts-stack.ts
@@ -2,11 +2,11 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { HitCounter } from './hitcounter';
-
 import { Construct } from 'constructs';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopTsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const hello = new lambda.Function(this, 'HelloHandler', {
@@ -26,6 +26,11 @@ export class CdkWorkshopTsStack extends cdk.Stack {
     // defines an API Gateway REST API resource backed by our "hello" function 
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
     });
 
 
